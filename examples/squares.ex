2eed3d5f224666c1afcca241f27570b3823143a9
Michael Berkowitz <michael.berkowitz@gmail.com>
2022-07-04 16:38:25 -0400

    Add squares example start

---
+++ examples/squares.ex
@@ -0,0 +1,43 @@
+alias Xairo.{ImageSurface, Context, Rgba}
+
+import Context
+
+scale = 50
+
+rando = fn -> :rand.uniform() / 4 - 0.125 end
+
+add_square = fn context, x, y -> rectangle(context, {x, y}, 1.5 + rando.(), 1.5 + rando.()) end
+
+surface = ImageSurface.create(:argb32, 60 * scale, 60 * scale)
+
+context = Context.new(surface)
+
+context =
+  context
+  |> scale(scale, scale)
+  |> set_source(Rgba.new(0.7, 0.8, 0.6))
+  |> paint()
+  |> set_source(Rgba.new(0.1, 0.1, 0.2))
+  |> set_line_width(0.1)
+
+:random.seed(1, 2, 3)
+
+points =
+  for _ <- 1..800 do
+    x = :random.uniform(round(60 / 2 - 6)) + 2
+    y = :random.uniform(round(60 / 2 - 6)) + 2
+    {x * 2, y * 2}
+  end
+  |> Enum.uniq()
+
+Enum.map(points, &elem(&1, 0)) |> Enum.min_max() |> IO.inspect()
+Enum.map(points, &elem(&1, 1)) |> Enum.min_max() |> IO.inspect()
+
+context =
+  Enum.reduce(points, context, fn {x, y}, context ->
+    add_square.(context, x, y)
+  end)
+
+context = stroke(context)
+
+ImageSurface.write_to_png(surface, "examples/squares.png")
